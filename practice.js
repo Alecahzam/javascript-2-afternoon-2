fa95f4249a02eceb64983248718ca7a942238e43
Alec <townsend.alec@gmail.com>
2019-01-08 15:12:09 -0600

    Completed nonextra problems

--- practice.js
+++ practice.js
@@ -18,8 +18,10 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
-
+function first(arr){
+  return arr[0]
+};
+first(arr);
 
 ////////// PROBLEM 2 //////////
 
@@ -31,9 +33,11 @@ var arr = [40,50,60];
   Create a function named 'last' that is given 'arr' as an argument. 
   Return the last item in the given array.
 */
-
 //Code Here
-
+function last(arr){
+  return arr[2]
+};
+last(arr);
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +52,13 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family){
+  for ( i = 0 ; i < family.length ;i++){
+  alert(family[i])
+  };
+};
+console.log(family)
+looper(family)
 
 
 ////////// PROBLEM 4 //////////
@@ -63,8 +73,13 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
-
+function reversedLooper(letters){
+  for (i= letters.length - 1; i >= 0; i--){
+  alert(letters[i])
+  };
+};
+console.log(letters);
+reversedLooper(letters);
 
 ////////// PROBLEM 5 //////////
 
@@ -79,6 +94,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
+var evenFinder = (nums) => {
+ var arrayfodder = [];
+   for (i = 0 ; i < nums.length ; i++){
+    if (nums[i] % 2 == 0)
+    arrayfodder.push(nums[i]);
+   }
+return arrayfodder;
+}
 
 
 
